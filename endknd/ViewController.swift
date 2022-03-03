6fc86a825b0dc68f68877762095adf0d68e816d9
Grace <gmilo92@haverfordsd.net>
2022-03-03 10:12:33 -0500

    Final project

--- endknd/ViewController.swift
+++ endknd/ViewController.swift
@@ -15,11 +15,13 @@ class ViewController: UIViewController
         
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        emojis.alpha = 0
     }
     
     @IBOutlet weak var outputLabel: UILabel!
     @IBOutlet weak var num1: UITextField!
     @IBOutlet weak var num2: UITextField!
+    @IBOutlet weak var emojis: UIImageView!
     
     
     
@@ -29,9 +31,19 @@ class ViewController: UIViewController
         var val2 = Double(num2.text!) ?? 0
         var finalval = val1 * val2
         var finalvalS = String(finalval)
+        var finalvalInt = Int(finalval+0.5)
         outputLabel.text = finalvalS
         num1.resignFirstResponder()
         num2.resignFirstResponder()
+        if finalvalInt%2 == 0
+        {
+            emojis.image = UIImage (named: "laugh1")
+            emojis.alpha = 1
+        }
+        else
+        {
+            emojis.image = UIImage (named: "laugh2")
+        }
     }
     
     @IBAction func clearButton(_ sender: Any)
