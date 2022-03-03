ccd57662bfdcf6287ecb8e84956314b6b26f6532
Grace <gmilo92@haverfordsd.net>
2022-03-03 09:40:57 -0500

    March 3

--- endknd/ViewController.swift
+++ endknd/ViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBOutlet weak var outputLabel: UILabel!
+    @IBOutlet weak var num1: UITextField!
+    @IBOutlet weak var num2: UITextField!
+    
+    
+    
+    @IBAction func multiplyPressed(_ sender: Any)
+    {
+        var val1 = Double(num1.text!) ?? 0
+        var val2 = Double(num2.text!) ?? 0
+        var finalval = val1 * val2
+        var finalvalS = String(finalval)
+        outputLabel.text = finalvalS
+    }
+    
 
 }
 
